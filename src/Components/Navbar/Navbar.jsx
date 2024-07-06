3ee21ee6b6fbf6d0f56e3f6a181a890de82943a6
lokesh pandey <luvpnd@gmail.com>
2024-07-06 13:27:58 +0530

    logic changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <h3>Habit Tracker</h3>
-      <div>Other Links</div>
+      <h3>
+        <Link className={styles.logo} to={"/"}>
+          Habit Tracker
+        </Link>
+      </h3>
     </div>
   );
 };
